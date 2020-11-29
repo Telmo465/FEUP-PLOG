c35cf69453431bd22b27cc92efa2fb35f50f3119
Telmo <up201806821@fe.up.pt>
2020-11-29 16:16:44 +0000

    Final version

--- src/play.pl
+++ src/play.pl
@@ -25,6 +25,20 @@ game_loop('BOT1', 'BOT1'):- %implements the main loop of the game
 	retract(state(_,_)),
 	endGame.
 
+%game loop --> PC x PC
+game_loop('BOT2', 'BOT2'):- %implements the main loop of the game
+	initial(GameState),
+	assert(state(1, GameState)),
+	repeat,
+		retract(state(Player, Board)),
+		choose_move(Board ,Player, easy, NewBoard), 
+		updatePlayer(Player, NextPlayer),
+		assert(state(NextPlayer, NewBoard)),
+		once(display_game(NewBoard, NextPlayer)), % displays the current state of the board
+		game_over(NewBoard, _),% checks if the game has ended
+	retract(state(_,_)),
+	endGame.
+
 %game loop --> Human x PC Hard
 game_loop('Player', 'BOT1'):- %implements the main loop of the game
 	initial(GameState),
@@ -159,8 +173,6 @@ game_over(GameState, 'Red'):-
 endGame:-
 	retract(winner(Player)),
     write('\n\n ________________________________________________________________________ \n'),
-    nl,
-	write('                                3 in a row!\n'),
 	symbol(Player,S),
     nl,
     write('                            '),                                   
