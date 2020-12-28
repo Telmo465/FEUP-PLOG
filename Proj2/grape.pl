086d8eaef7834004dd3560335167d7590a9f62dc
Caio-Nogueira <caion00@hotmail.com>
2020-12-28 19:57:59 +0000

    Output now being displayed correctly

--- Proj2/grape.pl
+++ Proj2/grape.pl
@@ -7,14 +7,23 @@
           [green,white],
            [white]
     ]
+*/
 
+%copied from SWI prolog
+flatten([], []) :- !. %transforms matrix into list
+flatten([L|Ls], FlatL) :-
+    !,
+    flatten(L, NewL),
+    flatten(Ls, NewLs),
+    append(NewL, NewLs, FlatL).
+flatten(L, [L]).
 
 list([[red,yellow,yellow,green],
     [white,red,white],
      [green,white],
       [white]
 ]).
-*/
+
 
 exactly(_, [], 0). /*slides aulas teoricas*/
 exactly(X, [Y|L], N) :-
@@ -24,7 +33,7 @@ exactly(X, [Y|L], N) :-
 
 
 
-list([[A,B,C,D,E,F],[G,H,I,J,B],[K,L,G,M],[N,O,L],[P,Q],[R]]).
+%list([[A,B,C,D,E,F],[G,H,I,J,B],[K,L,G,M],[N,O,L],[P,Q],[R]]).
 
 placeColorsConstraints(NumRows, _, NumRows, _, [R,G,Y]).
 placeColorsConstraints(Index, ColouredList, NumRows, List, [R,G,Y]):-
@@ -70,14 +79,7 @@ fillColouredRows(Index ,SubL, ColouredSubL, Len, [R,G,Y], ColouredList):-
 
 applyConstraints(List):-
     length(List, NumRows),
-    %domain([R,G,B],2,20000),
-    %placeColorsConstraints(0, ColouredList, NumRows, List, [R,G,B]),!,
-    %term_variables(ColouredList, FlatList),
-    %domain(FlatList, 1,20000),
     nth0(0, List, FirstRow),
-   /* write(NumRows), nl,
-    write(ColouredList), nl,*/
-    %all_distinct(ColouredList),
     domain(FirstRow, 1, 9),
     buildGrapeList(1, List, NumRows).
 
@@ -104,17 +106,107 @@ getCell(Index ,SubList, PreviousSubList, SubListLen):-
     getCell(NIndex, SubList, PreviousSubList, SubListLen).
 
 
-solver(List):-
+solver(Output):-
     list(List),
-    length(List, Len),
-    %length(NewList, Len),
-    %term_variables(NewFlatList, NewList),
-    %domain(NewFlatList, 1, 20000),
-    term_variables(List, FlatList),
-    all_distinct(FlatList),
+    flatten(List, FlatList1),
+    FlatList2 = FlatList1,
+    %write(FlatList), nl,
     
-    applyConstraints(List),
-    %write(List),nl,
-    term_variables(List, Output),
-    labeling([], Output).
+    length(List, NumRows),
 
+    domain([R,G,Y], 1,2000),
+    placeColorsConstraints(0, ColouredList, NumRows, List, [R,G,Y]),
+    
+    applyConstraints(ColouredList),
+    term_variables(ColouredList, Output),
+    all_distinct(Output),
+    
+    labeling([], Output), 
+    removeAllExceptWhite(FlatList1, [], List1),
+    
+    getColors(List1, Output, [], List1, ColorList),
+
+    %write(ColorList),
+    getWhiteCells(Output, [], FinalList, ColorList),
+    %write(FinalList),nl,
+    %write(List),
+    displayOutput(List, ColorList, FinalList,[], FinalSolution),
+    write(FinalSolution).
+    
+
+
+/*  Display the output in a human-friendly way */
+
+removeAllExceptWhite([], Solution, Solution).
+
+removeAllExceptWhite([H|List], FinalList, Solution):-
+    \+member(H, FinalList),
+    H \= white,
+    append(FinalList, [H], NewFinalList),
+    removeAllExceptWhite(List, NewFinalList, Solution).
+
+removeAllExceptWhite([white|List], FinalList, Solution):-
+    append(FinalList, [white], NewFinalList),
+    removeAllExceptWhite(List, NewFinalList, Solution).
+
+removeAllExceptWhite([H|List], FinalList, Solution):-
+    removeAllExceptWhite(List, FinalList, Solution).
+
+
+getColors([], FinalList, NewSolution,_, NewSolution).
+
+getColors([H|ColorList], FinalList, Solution, InitialList, FinalSolution):-
+    nth0(Index, InitialList, H),
+    H \= white,
+    \+member(H-_, Solution),
+    nth0(Index, FinalList, Elem),
+    append(Solution, [H-Elem], NewSolution),
+    getColors(ColorList, FinalList, NewSolution, InitialList, FinalSolution).
+
+
+getColors([white|ColorList], FinalList, Solution, InitialList, FinalSolution):-
+    getColors(ColorList, FinalList, Solution, InitialList, FinalSolution).
+
+getColors([_|ColorList], FinalList, Solution, InitialList, FinalSolution):-
+    getColors(ColorList, FinalList, Solution, InitialList, FinalSolution).
+
+
+
+getWhiteCells([], Solution, Solution, _).
+
+getWhiteCells([H|FlatList], WhiteList, FinalList, ColorList):-
+    \+member(_-H, ColorList),
+    append(WhiteList, [H], NewWhiteList),
+    getWhiteCells(FlatList, NewWhiteList, FinalList, ColorList).
+
+
+getWhiteCells([H|FlatList], WhiteList, FinalList, ColorList):-
+    getWhiteCells(FlatList, WhiteList, FinalList, ColorList).
+    
+
+displayOutput([], ColorList, WhiteList, FinalSolution, FinalSolution).
+
+displayOutput([Row|List], ColorList, WhiteList, Solution, FinalSolution):-
+    displayRows(Row, ColorList, WhiteList, [], FinalRow, NewWhiteList),
+    append(Solution, [FinalRow], NewFinalSolution),
+    write(FinalRow),nl,
+    displayOutput(List, ColorList, NewWhiteList, NewFinalSolution, FinalSolution).
+
+
+displayRows([], ColorList, NewWhiteList, Row, Row, NewWhiteList).
+
+displayRows([H|Row], ColorList, WhiteList, RowList, FinalRow, NewWhiteList):-
+    member(H-Num, ColorList),
+    %write(ColorList),nl,
+    
+    %write('estou aqui bro: Num= '), write(Num),nl,
+    append(RowList, [Num], NewRowList),
+    displayRows(Row, ColorList, WhiteList, NewRowList, FinalRow, NewWhiteList).
+
+
+displayRows([white|Row], ColorList, [H|WhiteList], RowList, FinalRow, NewWhiteList):-
+    append(RowList, [H], NewRowList),
+    displayRows(Row, ColorList, WhiteList, NewRowList, FinalRow, NewWhiteList).
+    
+
+    
